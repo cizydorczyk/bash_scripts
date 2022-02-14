5ba3a972d6a1e82d07763ca199878edb838865cf
cizydorczyk <conradizydorczyk@gmail.com>
2022-02-14 12:06:45 -0700

    updates

--- phd/k5-unmapped-reference.sh
+++ phd/k5-unmapped-reference.sh
@@ -35,55 +35,55 @@ done
 
 COMBFQDIR=${PROJECTDIR}combined-unmapped-reads/
 UNIDIR=${COMBFQDIR}unicycler/
-
+#
 # mkdir -p ${COMBFQDIR}
-
+#
 # echo "Combining unmapped reads from all samples..."
-
+#
 COMBINEDR1=${COMBFQDIR}${ST}-comb-unmapped_1.fastq
 COMBINEDR2=${COMBFQDIR}${ST}-comb-unmapped_2.fastq
-
+#
 # for i in $(cat ${ISOLATELIST})
 # do
 #     echo ${i}
 #     UNMAPPEDR1=${PROJECTDIR}raw_snippy_output/${i}/snps.unmapped_R1.fq.gz
 #     UNMAPPEDR2=${PROJECTDIR}raw_snippy_output/${i}/snps.unmapped_R2.fq.gz
-
+#
 #     zcat ${UNMAPPEDR1} >> ${COMBINEDR1}
 #     zcat ${UNMAPPEDR2} >> ${COMBINEDR2}
 # done
-
+#
 # gzip ${COMBINEDR1}
 # gzip ${COMBINEDR2}
-
+#
 # echo "Obtaining read counts of combined forward & reverse read files..."
-
+#
 # fix_base_count() {
 #     local counts=($(cat))
 #     echo " ${counts[0]} $((${counts[1]} - ${counts[0]}))"
 # }
-
+#
 # gzip -dc ${COMBINEDR1} \
 #     | awk 'NR % 4 == 2' \
 #     | wc -cl \
 #     | fix_base_count
-
+#
 # gzip -dc ${COMBINEDR2} \
 #     | awk 'NR % 4 == 2' \
 #     | wc -cl \
 #     | fix_base_count
-
+#
 # ### Assemble unmapped, combined reads using Unicycler:
 # echo "Assembling unmapped reads using Unicycler..."
-
+#
 # unicycler -1 ${COMBINEDR1}.gz -2 ${COMBINEDR2}.gz -o ${UNIDIR} --depth_filter 0.25 --min_fasta_len 100 --min_polish_size 100 -t 8
-
+#
 UNIASSEMBLY=${UNIDIR}assembly.fasta
 FILTASSEMBLY=${UNIDIR}${ST}-filtered-assembly.fasta
-
-# ### Filter assembled contigs based on length (move to python script? filtering pre-blast might be good though to remove garbage...):
-# echo "Filtering contigs shorter than minimum length provided..."
-
+#
+# # ### Filter assembled contigs based on length (move to python script? filtering pre-blast might be good though to remove garbage...):
+# # echo "Filtering contigs shorter than minimum length provided..."
+#
 # bioawk -v len=${MINCONTIGLEN} -c fastx '{ if(length($seq) > len) { print ">"$name; print $seq }}' ${UNIASSEMBLY} > ${FILTASSEMBLY}
 
 ### Blast assembled contigs & remove non-target genus contigs
@@ -91,7 +91,7 @@ RAWBLASTOUTPUT=${UNIDIR}${ST}-raw-blast-output.xml
 
 echo "Running blastn on contigs..."
 # blastn -db nt -query ${FILTASSEMBLY} -outfmt "6 qseqid sseqid sacc bitscore evalue pident length staxids sscinames" -out ${RAWBLASTOUTPUT} -evalue 0.00001 -num_threads 8
-blastn -db nt -query ${FILTASSEMBLY} -outfmt 5 -evalue 0.00001 -out ${RAWBLASTOUTPUT} -num_threads 8 -max_target_seqs 10
+blastn -db /media/conrad/Secondary_HDD/blastdb/nt -query ${FILTASSEMBLY} -outfmt 5 -evalue 0.00001 -out ${RAWBLASTOUTPUT} -num_threads 8 -max_target_seqs 10
 
 
 ### Parse blast output, keeping only contigs that have GENUS in at least one of their top 10 hits:
@@ -131,11 +131,3 @@ rast-export-genome gff < ${ANNGTO} > ${ANNGFF}
 rast-export-genome genbank < ${ANNGTO} > ${ANNGBK}
 
 echo "Done annotating."
-
-
-
-
-
-
-
-
